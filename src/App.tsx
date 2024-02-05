67d41a3eb16dbe6f4b537c2a28230d0a10293bae
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-05 19:52:59 +0530

    feat: added setting up player in useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView, StatusBar, StyleSheet, Text } from "react-native";
+import { addTracks, setupPlayer } from "../track-player-service";
 
 export default function App(): JSX.Element {
+  const [, setIsPlayerReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function setup(): Promise<boolean> {
+      const isSetup = await setupPlayer();
+      if (isSetup === true) {
+        await addTracks([
+          {
+            title: "Animal Walk bgm",
+            url: require("./assets/animal walk.mp3"),
+            artist: "Sandeep Reddy",
+            album: "Animal",
+            artwork: "./assets/animal-wallpaper.jpg",
+          },
+        ]);
+      }
+
+      return isSetup;
+    }
+
+    setup().then(setIsPlayerReady).catch(console.log);
+  }, []);
+
   return (
     <SafeAreaView>
       <StatusBar />
