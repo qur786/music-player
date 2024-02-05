61364a7a9d08b7357d42837235183ecd00751958
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-05 19:33:28 +0530

    refactor: added music player heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { SafeAreaView, StatusBar, View } from "react-native";
+import { SafeAreaView, StatusBar, StyleSheet, Text } from "react-native";
 
 export default function App(): JSX.Element {
   return (
     <SafeAreaView>
       <StatusBar />
-      <View>App</View>
+      <Text style={styles.heading}>Music Player</Text>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  heading: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingVertical: 8,
+  },
+});
