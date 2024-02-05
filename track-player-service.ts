a3cbef3c3635121a7d00a0d37cdfe2399b150499
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-05 19:44:46 +0530

    refactor: add setupPlayer and addTracks functions

--- track-player-service.ts
+++ track-player-service.ts
@@ -1,5 +1,23 @@
-import TrackPlayer, { Event } from "react-native-track-player";
-import type { ServiceHandler } from "react-native-track-player";
+import TrackPlayer, { Event, RepeatMode } from "react-native-track-player";
+import type { ServiceHandler, Track } from "react-native-track-player";
+
+export async function setupPlayer(): Promise<boolean> {
+  let isSetup = false;
+  try {
+    await TrackPlayer.getActiveTrack();
+    isSetup = true;
+  } catch {
+    await TrackPlayer.setupPlayer();
+    isSetup = true;
+  }
+
+  return isSetup;
+}
+
+export async function addTracks(tracks: Track[]): Promise<void> {
+  await TrackPlayer.add(tracks);
+  await TrackPlayer.setRepeatMode(RepeatMode.Queue);
+}
 
 export const trackPlayerPlaybackService: ServiceHandler = async () => {
   TrackPlayer.addEventListener(Event.RemoteNext, () => {
