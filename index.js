5c75ce48c72595943f837c8741688f21ba3c9e35
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-05 19:39:39 +0530

    feat: add event handlers for tracker player in service handler

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
  */
 
 import { AppRegistry } from "react-native";
+import TrackPlayer from "react-native-track-player";
 import { name as appName } from "./app.json";
 import App from "./src/App";
-import TrackPlayer from "react-native-track-player";
 import { trackPlayerPlaybackService } from "./track-player-service";
 
 AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(() => trackPlayerPlaybackService());
+TrackPlayer.registerPlaybackService(() => trackPlayerPlaybackService);
--- track-player-service.ts
+++ track-player-service.ts
@@ -1,5 +1,17 @@
+import TrackPlayer, { Event } from "react-native-track-player";
 import type { ServiceHandler } from "react-native-track-player";
 
-export function trackPlayerPlaybackService(): ServiceHandler {
-  return async () => {};
-}
+export const trackPlayerPlaybackService: ServiceHandler = async () => {
+  TrackPlayer.addEventListener(Event.RemoteNext, () => {
+    TrackPlayer.skipToNext();
+  });
+  TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+    TrackPlayer.skipToPrevious();
+  });
+  TrackPlayer.addEventListener(Event.RemotePause, () => {
+    TrackPlayer.play();
+  });
+  TrackPlayer.addEventListener(Event.RemotePlay, () => {
+    TrackPlayer.pause();
+  });
+};
