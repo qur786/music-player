83187368b9689d2a099932b40a857e6340660e44
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-05 23:55:55 +0530

    feat: added song slider

---
+++ src/components/SongSlider/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Slider from "@react-native-community/slider";
+import { useProgress } from "react-native-track-player";
+
+export function SongSlider(): JSX.Element {
+  const { duration, position } = useProgress();
+  return (
+    <View>
+      <Slider
+        value={position}
+        maximumValue={duration}
+        minimumValue={0}
+        thumbTintColor="#FFFFFF"
+      />
+      <View>
+        <Text>{new Date(position * 1000).toISOString().substring(15, 19)}</Text>
+      </View>
+    </View>
+  );
+}
