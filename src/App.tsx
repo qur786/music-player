2725f180edb95717d3ccd8dcc88557b9eabc4e69
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-06 20:40:36 +0530

    refactor: update styling in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import {
   ActivityIndicator,
+  Dimensions,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
 } from "react-native";
-import { Controllers } from "./components/Controllers";
+import { Player } from "./components/Player";
 import { addTracks, setupPlayer } from "../track-player-service";
+
+const { height } = Dimensions.get("window");
 
 export default function App(): JSX.Element {
   const [isPlayerReady, setIsPlayerReady] = useState<boolean>(false);
@@ -22,7 +25,7 @@ export default function App(): JSX.Element {
             url: require("../assets/animal-walk.mp3"),
             artist: "Sandeep Reddy",
             album: "Animal",
-            artwork: "./assets/animal-wallpaper.jpg",
+            artwork: "assets/animal-wallpaper.jpg",
           },
         ]);
       }
@@ -37,19 +40,26 @@ export default function App(): JSX.Element {
     <SafeAreaView style={styles.main}>
       <StatusBar />
       <Text style={styles.heading}>Music Player</Text>
-      {isPlayerReady ? <Controllers /> : <ActivityIndicator />}
+      {isPlayerReady ? <Player /> : <ActivityIndicator />}
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   main: {
-    height: "100%",
+    height,
+    backgroundColor: "#8395A7",
+    paddingVertical: 10,
+    flexDirection: "column",
+    alignItems: "center",
+    gap: 10,
   },
   heading: {
     textAlign: "center",
-    fontSize: 24,
+    color: "white",
+    fontSize: 32,
     fontWeight: "bold",
     paddingVertical: 8,
+    fontFamily: "cursive",
   },
 });
