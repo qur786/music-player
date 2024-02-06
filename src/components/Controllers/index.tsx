107a614b112942120bb6aa3f5f35c0f82661d480
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-06 21:34:19 +0530

    feat: add repeat mode icons

--- src/components/Controllers/index.tsx
+++ src/components/Controllers/index.tsx
@@ -1,13 +1,21 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import type { PressableProps } from "react-native";
 import { View, StyleSheet, Pressable } from "react-native";
 import TrackPlayer, {
+  RepeatMode,
   State,
   usePlaybackState,
 } from "react-native-track-player";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
+const RepeatModeIcons: Record<RepeatMode, string> = {
+  [RepeatMode.Off]: "sync-disabled",
+  [RepeatMode.Queue]: "sync",
+  [RepeatMode.Track]: "repeat-one",
+};
+
 export function Controllers(): JSX.Element {
+  const [repeatMode, setRepeatMode] = useState<RepeatMode>(RepeatMode.Off);
   const playbackState = usePlaybackState();
 
   const handlePrevPress: PressableProps["onPress"] = async () => {
@@ -31,30 +39,47 @@ export function Controllers(): JSX.Element {
         }
       }
     }, [playbackState]);
+
+  useEffect(() => {
+    TrackPlayer.getRepeatMode().then(setRepeatMode).catch(console.log);
+  }, []);
   return (
-    <View style={styles.playerContainer}>
-      <Pressable onPress={handlePrevPress}>
-        <Icon name="skip-previous" size={40} color="white" />
+    <View style={styles.container}>
+      <Pressable>
+        <Icon name={"share"} size={20} color="#2ECC72" />
       </Pressable>
-      <Pressable onPress={handleTogglePlayPause}>
-        <Icon
-          name={
-            playbackState.state === State.Playing
-              ? "pause-circle"
-              : "play-circle"
-          }
-          size={60}
-          color="#2ECC72"
-        />
-      </Pressable>
-      <Pressable onPress={handleNextPress}>
-        <Icon name="skip-next" size={40} color="white" />
+      <View style={styles.playerContainer}>
+        <Pressable onPress={handlePrevPress}>
+          <Icon name="skip-previous" size={40} color="white" />
+        </Pressable>
+        <Pressable onPress={handleTogglePlayPause}>
+          <Icon
+            name={
+              playbackState.state === State.Playing
+                ? "pause-circle"
+                : "play-circle"
+            }
+            size={60}
+            color="#2ECC72"
+          />
+        </Pressable>
+        <Pressable onPress={handleNextPress}>
+          <Icon name="skip-next" size={40} color="white" />
+        </Pressable>
+      </View>
+      <Pressable>
+        <Icon name={RepeatModeIcons[repeatMode]} size={20} color="#2ECC72" />
       </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
   playerContainer: {
     flexDirection: "row",
     justifyContent: "center",
