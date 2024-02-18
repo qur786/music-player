f934be8f05cc1f31beb57c7f6d101569db8f2208
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-19 00:23:27 +0530

    refactor: minor

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 /**
  * Function to format time in seconds to hh:mm:ss format.
  */
-export function formatTime(seconds: number): string {
+export function formatDuration(seconds: number): string {
   const totalMin = Math.floor(seconds / 60);
   const sec = Math.floor(seconds % 60);
   const hr = Math.floor(totalMin / 60);
