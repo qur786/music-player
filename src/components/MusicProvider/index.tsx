f3e19a0ce4d2f0f0ebf64e9bfaeae6231fdae2a7
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-19 13:49:56 +0530

    refactor: move setting up player to MusicProvider

--- src/components/MusicProvider/index.tsx
+++ src/components/MusicProvider/index.tsx
@@ -1,5 +1,6 @@
 import type { PropsWithChildren } from "react";
 import type { Track } from "react-native-track-player";
+import { setupPlayer } from "../../../track-player-service";
 import { Dirs, FileSystem } from "react-native-file-access";
 import { PermissionsAndroid, Platform } from "react-native";
 import React, {
@@ -33,94 +34,105 @@ const MusicContext = createContext<MusicContext>({
 export function MusicProvider({ children }: PropsWithChildren): JSX.Element {
   const [tracks, setTracks] = useState<Track[]>([]);
   const [loading, setLoading] = useState(false);
+  const [isPlayerSetup, setIsPlayerSetup] = useState(false);
+
+  useEffect(() => {
+    setupPlayer().then(setIsPlayerSetup).catch(console.log);
+    // TODO: notify user on setup failure
+  }, []); // Needs to setup player before any interaction with the player
 
   const requestRefetch: MusicContext["requestRefetch"] =
     useCallback(async () => {
-      setLoading(true);
-
       let output: Track[] = [];
-      let isMusicFilesReadPermissions = false;
-      let isExternalStorageReadPermissions = false; // TODO: remove this permission
 
-      if (Platform.OS === "android") {
-        isMusicFilesReadPermissions = await PermissionsAndroid.check(
-          "android.permission.READ_MEDIA_AUDIO"
-        );
+      if (isPlayerSetup) {
+        setLoading(true);
 
-        isExternalStorageReadPermissions = await PermissionsAndroid.check(
-          "android.permission.READ_EXTERNAL_STORAGE"
-        );
+        let isMusicFilesReadPermissions = false;
+        let isExternalStorageReadPermissions = false; // TODO: remove this permission
 
-        if (isMusicFilesReadPermissions === false) {
-          const result = await PermissionsAndroid.request(
-            "android.permission.READ_MEDIA_AUDIO",
-            {
-              title: "Music player audio files read permission.",
-              message: "App needs to access local audio files.",
-              buttonPositive: "Ask me later",
-              buttonNegative: "Cancel",
-              buttonNeutral: "Ok",
-            }
+        if (Platform.OS === "android") {
+          isMusicFilesReadPermissions = await PermissionsAndroid.check(
+            "android.permission.READ_MEDIA_AUDIO"
           );
 
-          isMusicFilesReadPermissions =
-            result === PermissionsAndroid.RESULTS.GRANTED;
-        }
-
-        if (isExternalStorageReadPermissions === false) {
-          const result = await PermissionsAndroid.request(
-            "android.permission.READ_EXTERNAL_STORAGE",
-            {
-              title: "Music player external storage permission.",
-              message: "App needs to access local files.",
-              buttonPositive: "Ask me later",
-              buttonNegative: "Cancel",
-              buttonNeutral: "Ok",
-            }
+          isExternalStorageReadPermissions = await PermissionsAndroid.check(
+            "android.permission.READ_EXTERNAL_STORAGE"
           );
-          isExternalStorageReadPermissions =
-            result === PermissionsAndroid.RESULTS.GRANTED;
+
+          if (isMusicFilesReadPermissions === false) {
+            const result = await PermissionsAndroid.request(
+              "android.permission.READ_MEDIA_AUDIO",
+              {
+                title: "Music player audio files read permission.",
+                message: "App needs to access local audio files.",
+                buttonPositive: "Ask me later",
+                buttonNegative: "Cancel",
+                buttonNeutral: "Ok",
+              }
+            );
+
+            isMusicFilesReadPermissions =
+              result === PermissionsAndroid.RESULTS.GRANTED;
+          }
+
+          if (isExternalStorageReadPermissions === false) {
+            const result = await PermissionsAndroid.request(
+              "android.permission.READ_EXTERNAL_STORAGE",
+              {
+                title: "Music player external storage permission.",
+                message: "App needs to access local files.",
+                buttonPositive: "Ask me later",
+                buttonNegative: "Cancel",
+                buttonNeutral: "Ok",
+              }
+            );
+            isExternalStorageReadPermissions =
+              result === PermissionsAndroid.RESULTS.GRANTED;
+          }
         }
-      }
 
-      // TODO: add permission handler for other OS when creating app for those envs.
+        // TODO: add permission handler for other OS when creating app for those envs.
 
-      if (isMusicFilesReadPermissions) {
-        const songsResults = await getAll({
-          minSongDuration: 1000,
-          sortOrder: SortSongOrder.DESC,
-          sortBy: SortSongFields.TITLE,
-        });
+        if (isMusicFilesReadPermissions) {
+          const songsResults = await getAll({
+            minSongDuration: 1000,
+            sortOrder: SortSongOrder.DESC,
+            sortBy: SortSongFields.TITLE,
+          });
 
-        if (typeof songsResults === "string") {
-          console.log(songsResults);
-          // TODO: notify user that could not find any music files
+          if (typeof songsResults === "string") {
+            console.log(songsResults);
+            // TODO: notify user that could not find any music files
+          } else {
+            // TODO: use try catch
+            output = convertMusicFileToTrack(songsResults);
+            await mergeQueueTracks(output);
+            await FileSystem.writeFile(
+              Dirs.DocumentDir + MUSIC_FILE_PATH,
+              JSON.stringify(output)
+            );
+            setTracks(output);
+          }
         } else {
-          // TODO: use try catch
-          output = convertMusicFileToTrack(songsResults);
-          await mergeQueueTracks(output);
-          await FileSystem.writeFile(
-            Dirs.DocumentDir + MUSIC_FILE_PATH,
-            JSON.stringify(output)
-          );
-          setTracks(output);
+          // TODO: notify user that user needs to allow permission to access music files.
         }
-      } else {
-        // TODO: notify user that user needs to allow permission to access music files.
+        setLoading(false);
       }
-      setLoading(false);
       return output;
-    }, []);
+    }, [isPlayerSetup]);
 
   useEffect(() => {
-    FileSystem.readFile(Dirs.DocumentDir + MUSIC_FILE_PATH)
-      .then((data) => {
-        const parsedTracks = JSON.parse(data) as Track[];
-        setTracks(parsedTracks);
-        mergeQueueTracks(parsedTracks);
-      })
-      .catch(console.log);
-  }, []);
+    if (isPlayerSetup) {
+      FileSystem.readFile(Dirs.DocumentDir + MUSIC_FILE_PATH)
+        .then((data) => {
+          const parsedTracks = JSON.parse(data) as Track[];
+          setTracks(parsedTracks);
+          mergeQueueTracks(parsedTracks);
+        })
+        .catch(console.log);
+    }
+  }, [isPlayerSetup]);
 
   return (
     <MusicContext.Provider value={{ requestRefetch, tracks, loading }}>
