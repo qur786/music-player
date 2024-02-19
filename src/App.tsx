b8fcd9270ec30f80ebc34a22e48b6aa11fc51619
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-19 13:47:58 +0530

    refactor: move setup player to MusicProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,17 @@ import { Home } from "./screens/Home";
 import { MusicProvider } from "./components/MusicProvider";
 import { NavigationContainer } from "@react-navigation/native";
 import { Playlist } from "./screens/Playlist";
+import React from "react";
 import type { RootStackParamList } from "./routes";
 import { Routes } from "./routes";
 import { StyleSheet } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { setupPlayer } from "../track-player-service";
-import React, { useEffect } from "react";
 
 const { Navigator, Screen } = createNativeStackNavigator<RootStackParamList>();
 
 const { height } = Dimensions.get("window");
 
 export default function App(): JSX.Element {
-  useEffect(() => {
-    setupPlayer().then(console.log).catch(console.log);
-  }, []); // Needs to setup player before any interaction with the player
-
   return (
     <MusicProvider>
       <NavigationContainer>
