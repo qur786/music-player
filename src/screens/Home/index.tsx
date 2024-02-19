98ee41aa170ba7b1677a066d3fec1996cc07c661
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-19 11:58:47 +0530

    refactor: added button for playlist navigation from Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,23 @@
+import type { HomeProps } from "../../routes";
 import { Player } from "../../components/Player";
-import { setupPlayer } from "../../../track-player-service";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
-import React, { useEffect, useState } from "react";
+import type { PressableProps } from "react-native";
+import React from "react";
+import { Routes } from "../../routes";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export function Home(): JSX.Element {
-  const [isPlayerReady, setIsPlayerReady] = useState<boolean>(false);
-
-  useEffect(() => {
-    setupPlayer().then(setIsPlayerReady).catch(console.log);
-  }, []);
-
+export function Home({ navigation }: HomeProps): JSX.Element {
+  const handlePlaylistNavigation: PressableProps["onPress"] = () => {
+    navigation.navigate(Routes.Playlist);
+  };
   return (
     <View style={styles.main}>
-      <Text style={styles.heading}>Music Player</Text>
-      {isPlayerReady ? <Player /> : <ActivityIndicator />}
+      <Pressable onPress={handlePlaylistNavigation}>
+        <Text>Playlist</Text>
+      </Pressable>
+      <Text style={styles.heading}>
+        <Text>Music Player</Text>
+      </Text>
+      <Player />
     </View>
   );
 }
