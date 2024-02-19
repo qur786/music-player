e9b977f1f99005566b0784476dc55e145010e5b5
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-19 17:15:37 +0530

    feat: added refresh button

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import { Dimensions } from "react-native";
 import { Home } from "./screens/Home";
-import { MusicProvider } from "./components/MusicProvider";
 import { NavigationContainer } from "@react-navigation/native";
 import { Playlist } from "./screens/Playlist";
+import type { PressableProps } from "react-native";
 import React from "react";
 import type { RootStackParamList } from "./routes";
 import { Routes } from "./routes";
 import { StyleSheet } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Dimensions, Pressable, Text } from "react-native";
+import { MusicProvider, useMusicFiles } from "./components/MusicProvider";
 
 const { Navigator, Screen } = createNativeStackNavigator<RootStackParamList>();
 
 const { height } = Dimensions.get("window");
+
+function HeaderRefreshButton(): JSX.Element {
+  const { requestRefetch } = useMusicFiles();
+
+  const handleRefreshPress: PressableProps["onPress"] = () => {
+    requestRefetch().catch(console.log);
+  };
+  return (
+    <Pressable onPress={handleRefreshPress}>
+      <Text>Reload</Text>
+      {/* TODO: use icon */}
+    </Pressable>
+  );
+}
 
 export default function App(): JSX.Element {
   return (
@@ -20,11 +35,18 @@ export default function App(): JSX.Element {
         <Navigator
           initialRouteName={Routes.Playlist}
           screenOptions={{
-            headerShown: false,
             contentStyle: styles.main,
+            headerSearchBarOptions: {},
+            headerTitleAlign: "center",
           }}>
           <Screen name={Routes.Home} component={Home} />
-          <Screen name={Routes.Playlist} component={Playlist} />
+          <Screen
+            name={Routes.Playlist}
+            component={Playlist}
+            options={{
+              headerRight: HeaderRefreshButton,
+            }}
+          />
         </Navigator>
       </NavigationContainer>
     </MusicProvider>
