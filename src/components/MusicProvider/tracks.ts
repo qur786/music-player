a6464d1b39f051f6398518258e06087cff4d2769
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-19 13:48:39 +0530

    docs: added todo comment to handle error

--- src/components/MusicProvider/tracks.ts
+++ src/components/MusicProvider/tracks.ts
@@ -6,14 +6,13 @@ type MusicFile = Exclude<Awaited<ReturnType<typeof getAll>>, string>;
 
 export async function mergeQueueTracks(tracks: Track[]) {
   const currentQueue = await TrackPlayer.getQueue();
-  console.log(currentQueue);
   const newQueue = [...tracks, ...currentQueue].sort((a, b) =>
     a.title && b.title
       ? a.title.localeCompare(b.title)
       : a.url.localeCompare(b.url)
   );
   await TrackPlayer.setQueue(newQueue);
-}
+} // TODO: fix it
 
 export function convertMusicFileToTrack(musicFiles: MusicFile): Track[] {
   const tracks: Track[] = musicFiles.map<Track>(
@@ -24,7 +23,8 @@ export function convertMusicFileToTrack(musicFiles: MusicFile): Track[] {
       genre,
       duration: duration / 1000,
       artist,
-      artwork: cover,
+      artwork:
+        typeof cover === "string" && cover.length > 0 ? cover : undefined,
     })
   );
 
