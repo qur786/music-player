640a72e4637276c90728a34c4fc2ec9b0ae1bef0
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-19 20:35:19 +0530

    refactor: remove requestRefresh from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
 import { Home } from "./screens/Home";
+import { MusicProvider } from "./components/MusicProvider";
 import { NavigationContainer } from "@react-navigation/native";
 import { Playlist } from "./screens/Playlist";
-import type { PressableProps } from "react-native";
 import React from "react";
 import type { RootStackParamList } from "./routes";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MusicProvider, useMusicFiles } from "./components/MusicProvider";
-import { Pressable, Text } from "react-native";
 import { Routes, RoutesIcons } from "./routes";
 
 const { Navigator, Screen } = createBottomTabNavigator<RootStackParamList>();
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function HeaderRefreshButton(): JSX.Element {
-  const { requestRefetch } = useMusicFiles();
-
-  const handleRefreshPress: PressableProps["onPress"] = () => {
-    requestRefetch().catch(console.log);
-  };
-  return (
-    <Pressable
-      onPress={handleRefreshPress}
-      android_ripple={{ color: "#E74292" }}>
-      <Text>Reload</Text>
-      {/* TODO: use icon */}
-    </Pressable>
-  );
-}
 
 export default function App(): JSX.Element {
   return (
