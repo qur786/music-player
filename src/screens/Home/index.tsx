47c601922b9f0942e2f019ec36064d05d6ec68c8
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-19 20:20:38 +0530

    refactor: remove playlist button from the home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,10 @@
-import type { HomeProps } from "../../routes";
 import { Player } from "../../components/Player";
-import type { PressableProps } from "react-native";
 import React from "react";
-import { Routes } from "../../routes";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-export function Home({ navigation }: HomeProps): JSX.Element {
-  const handlePlaylistNavigation: PressableProps["onPress"] = () => {
-    navigation.navigate(Routes.Playlist);
-  };
+export function Home(): JSX.Element {
   return (
     <View style={styles.main}>
-      <Pressable onPress={handlePlaylistNavigation}>
-        <Text>Playlist</Text>
-      </Pressable>
       <Text style={styles.heading}>
         <Text>Music Player</Text>
       </Text>
