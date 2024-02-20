1b8451fefe5eb21964ec0ce64bc422799ec15233
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-21 00:53:12 +0530

    refactor: added SafeAreaView

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Playlist } from "./screens/Playlist";
 import React from "react";
 import type { RootStackParamList } from "./routes";
+import { SafeAreaView } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Routes, RoutesIcons } from "./routes";
 
@@ -11,37 +12,39 @@ const { Navigator, Screen } = createBottomTabNavigator<RootStackParamList>();
 
 export default function App(): JSX.Element {
   return (
-    <MusicProvider>
-      <NavigationContainer>
-        <Navigator
-          initialRouteName={Routes.Home}
-          screenOptions={{
-            tabBarActiveTintColor: "#E74292",
-            tabBarHideOnKeyboard: true,
-            headerTitleAlign: "center",
-            headerTitle: "Rhythmiq",
-            headerTitleStyle: {
-              fontFamily: "cursive",
-              fontSize: 24,
-              color: "#E74292",
-            },
-          }}>
-          <Screen
-            name={Routes.Home}
-            component={Home}
-            options={{
-              tabBarIcon: RoutesIcons.Home,
-            }}
-          />
-          <Screen
-            name={Routes.Playlist}
-            component={Playlist}
-            options={{
-              tabBarIcon: RoutesIcons.Playlist,
-            }}
-          />
-        </Navigator>
-      </NavigationContainer>
-    </MusicProvider>
+    <SafeAreaView style={{ flex: 1 }}>
+      <MusicProvider>
+        <NavigationContainer>
+          <Navigator
+            initialRouteName={Routes.Home}
+            screenOptions={{
+              tabBarActiveTintColor: "#E74292",
+              tabBarHideOnKeyboard: true,
+              headerTitleAlign: "center",
+              headerTitle: "Rhythmiq",
+              headerTitleStyle: {
+                fontFamily: "cursive",
+                fontSize: 24,
+                color: "#E74292",
+              },
+            }}>
+            <Screen
+              name={Routes.Home}
+              component={Home}
+              options={{
+                tabBarIcon: RoutesIcons.Home,
+              }}
+            />
+            <Screen
+              name={Routes.Playlist}
+              component={Playlist}
+              options={{
+                tabBarIcon: RoutesIcons.Playlist,
+              }}
+            />
+          </Navigator>
+        </NavigationContainer>
+      </MusicProvider>
+    </SafeAreaView>
   );
 }
