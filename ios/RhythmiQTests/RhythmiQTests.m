0670c1360dcbb77e41dacdeb90613c6b7fc940e1
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-22 23:07:03 +0530

    refactor: fix app name issue

--- ios/RhythmiQTests/RhythmiQTests.m
+++ ios/RhythmiQTests/RhythmiQTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MusicPlayerTests : XCTestCase
+@interface RhythmiQTests : XCTestCase
 
 @end
 
-@implementation MusicPlayerTests
+@implementation RhythmiQTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
